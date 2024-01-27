12cf5b33c7f2ed667baa8dba44c0b1fb3282e4f5
François "Kurikawa <francois@kurikawa.fr>
2024-01-28 00:09:38 +0100

    Link, javadoc

--- src/fr/kurikawa/linkedlist/Link.java
+++ src/fr/kurikawa/linkedlist/Link.java
@@ -1,17 +1,36 @@
 package fr.kurikawa.linkedlist;
 
+/**
+ * Maillon (Link)
+ * 
+ * Représente un maillon dans une liste doublement chaînée
+ * 
+ * @author François "Kurikawa"
+ */
 public class Link {
     
     private Object value;
     private Link previousLink;
     private Link nextLink;
 
+    /**
+     * Instancie un nouveau Maillon
+     * 
+     * @param value la valeur du maillon
+     * @param previousLink le maillon précédent
+     * @param nextLink le maillon suivant
+     */
     public Link(Object value, Link previousLink, Link nextLink){
         this.value = value;
         this.previousLink = previousLink;
         this.nextLink = nextLink;
     }
 
+    /**
+     * Instancie un nuveau Maillon
+     * 
+     * @param value la valeur du maillon
+     */
     public Link(Object value){
         this(value, null, null);
     }
@@ -40,6 +59,9 @@ public class Link {
         this.previousLink = newPrevious;
     }
 
+    /**
+     * Délie le maillon de son prédécesseur et de son suivant
+     */
     public void unlink(){
         this.nextLink = this.previousLink = null;
     }
