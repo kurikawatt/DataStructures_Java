00a00a78b33f60b7a7699384c781fdddf1e9aedf
François "Kurikawa <francois@kurikawa.fr>
2024-01-28 00:05:33 +0100

    Link, toString()

--- src/fr/kurikawa/linkedlist/Link.java
+++ src/fr/kurikawa/linkedlist/Link.java
@@ -44,4 +44,9 @@ public class Link {
         this.nextLink = this.previousLink = null;
     }
 
+    @Override 
+    public String toString(){
+        return "(" + this.value.toString() + ")";
+    }
+
 }
