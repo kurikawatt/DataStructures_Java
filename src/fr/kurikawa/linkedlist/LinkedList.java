0f0329f9e6863246196658086ac4ec1b88b02339
François "Kurikawa <francois@kurikawa.fr>
2024-01-28 00:14:44 +0100

    LinkedList, attributs et constructeurs

---
+++ src/fr/kurikawa/linkedlist/LinkedList.java
@@ -0,0 +1,15 @@
+package fr.kurikawa.linkedlist;
+
+public class LinkedList {
+    
+    private Link firstLink;
+    private Link lastLink;
+    private int length;
+
+    public LinkedList(){
+        this.firstLink = null;
+        this.lastLink = null;
+        this.length = 0; 
+    }
+
+}
